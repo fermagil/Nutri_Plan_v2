02e6e0b076140d131d334433bc6b005aa433de4f
fermagil <fermagil@gmail.com>
2025-05-29 14:27:31 +0200

    Update calculations.js

--- calculations.js
+++ calculations.js
@@ -5394,13 +5394,14 @@ if (!isNaN(results.pesoIdeal) && !isNaN(data.peso)) {
 						        resultElements.aguacorporal.textContent = `${actKgRedondeado} kg / ${porcentajeACTRedondeado}%`;
 							//updateElement('aguacorporal', resultadoAgua.aguacorporal, 1); 
 						        resultElements.aguacorporalSource.textContent = `Rango de referencia: ${resultadoAgua.rangoReferencia} (${resultadoAgua.fuente}; InBody USA). Estado: ${resultadoAgua.clasificacion}`;
+							    resultElements.aguacorporal.textContent = `${actKgRedondeado} kg / ${porcentajeACTRedondeado}%`;
+							//resultElements.aguacorporalSource.textContent = `Rango de referencia: ${resultadoAgua.rangoReferencia} (${resultadoAgua.fuente}; InBody USA). Estado: ${resultadoAgua.clasificacion}`;
+							updateElement('aguacorporal', `${actKgRedondeado} kg / ${porcentajeACTRedondeado}%`);
+							updateElement('aguacorporalSource', `Rango de referencia: ${resultadoAgua.rangoReferencia} (${resultadoAgua.fuente}; InBody USA). Estado: ${resultadoAgua.clasificacion}`);
+						// Suponiendo que resultadoAgua es el valor que quieres mostrar
 						    }
 						}
-						resultElements.aguacorporal.textContent = `${actKgRedondeado} kg / ${porcentajeACTRedondeado}%`;
-						resultElements.aguacorporalSource.textContent = `Rango de referencia: ${resultadoAgua.rangoReferencia} (${resultadoAgua.fuente}; InBody USA). Estado: ${resultadoAgua.clasificacion}`;
-						updateElement('aguacorporal', `${actKgRedondeado} kg / ${porcentajeACTRedondeado}%`);
-						updateElement('aguacorporalSource', `Rango de referencia: ${resultadoAgua.rangoReferencia} (${resultadoAgua.fuente}; InBody USA). Estado: ${resultadoAgua.clasificacion}`);
-						// Suponiendo que resultadoAgua es el valor que quieres mostrar
+						
 				                // Update IMC
 				                updateElement('imc', results.imc, 1); // Matches resultElements.imc
 				                if (resultElements.imcSource) { // Use imcSource to match resultElements
