50b88b013f560f66947c57e0b6264665cd4bcd17
fermagil <fermagil@gmail.com>
2025-06-01 23:12:13 +0200

    Update calculations.js

--- calculations.js
+++ calculations.js
@@ -4067,7 +4067,7 @@ if (!isNaN(results.pesoIdeal) && !isNaN(data.peso)) {
 				            resultados.porcentajeGrasa = datos.porcentajeGrasaActual;
 				            console.log(`[calcularGrasaVisceral] Usando %grasa proporcionado: ${resultados.porcentajeGrasa}%`);
 				        } else if  ( datos.pliegue_tricipital && datos.pliegue_subescapular && datos.pliegue_bicipital && datos.pliegue_suprailiaco) {
-				            resultados.porcentajeGrasa = calculateJacksonPollockBodyFat( datos.pliegue_tricipital, datos.pliegue_subescapular , datos.pliegue_bicipital , datos.pliegue_suprailiaco, edad);
+				            resultados.porcentajeGrasa = calculateJacksonPollockBodyFat( datos.pliegue_tricipital && datos.pliegue_subescapular && datos.pliegue_bicipital && datos.pliegue_suprailiaco && edad);
 				            console.log(`[calcularGrasaVisceral] %grasa calculado por Jackson-Pollock: ${resultados.porcentajeGrasa}%`);
 				        } else {
 				            throw new Error('Para deportistas se requiere % de grasa o medidas de pliegues');
