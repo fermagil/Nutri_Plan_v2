74d01241aaf3fb8e28bed0113d5b41aca69d4255
fermagil <fermagil@gmail.com>
2025-06-06 16:52:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1169,7 +1169,7 @@ document.addEventListener('DOMContentLoaded', function() {
         { input: 'alt', result: 'result-alt', source: 'alt-source', label: 'ALT', unit: 'U/L', range: [5, 200] },
         { input: 'ggt', result: 'result-ggt', source: 'ggt-source', label: 'GGT', unit: 'U/L', range: [5, 300] },
         { input: 'creatinina', result: 'creatinina', source: 'creatinina-source', label: 'Creatinina', unit: 'mg/dL', range: [0.5, 1.5] },
-        { input: 'bun', result: 'result-bun', source: 'bun-source', label: 'BUN', unit: 'mg/dL', range: [7, 25] },
+        { input: 'bun', result: 'bun', source: 'bun-source', label: 'BUN', unit: 'mg/dL', range: [7, 25] },
         { input: 'tsh', result: 'result-tsh', source: 'tsh-source', label: 'TSH', unit: 'µIU/mL', range: [0.05, 15] },
         { input: 'testosterona', result: 'result-testosterona', source: 'testosterona-source', label: 'Testosterona', unit: 'ng/dL', range: [20, 1500] },
         { input: 'cortisol', result: 'cortisol', source: 'cortisol-source', label: 'Cortisol', unit: 'µg/dL', range: [5, 25] },
